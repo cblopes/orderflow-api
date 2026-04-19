8f83a5c458294aa0a1c2a009240a9c8dcdb9f93e
Christhian Lopes <christhiancbl@gmail.com>
2026-04-19 00:51:04 -0300

    feat(money): add value object money

---
+++ src/OrderFlow.Domain/ValueObjects/Money.cs
@@ -0,0 +1,28 @@
+namespace OrderFlow.Domain.ValueObjects;
+
+public sealed class Money : ValueObject
+{
+    public decimal Value { get; private set; }
+
+    private Money() { }
+
+    public Money(decimal value)
+    {
+        if (value < 0)
+            throw new ArgumentException("Price cannot be negative.");
+
+        Value = Math.Round(value, 4);
+    }
+
+    public static Money Create(decimal value)
+        => new(value);
+
+    public override string ToString()
+        => Value.ToString("C");
+
+    public static Money operator +(Money a, Money b)
+        => new(a.Value + b.Value);
+
+    public static Money operator *(Money a, int quantity)
+        => new(a.Value * quantity);
+}
