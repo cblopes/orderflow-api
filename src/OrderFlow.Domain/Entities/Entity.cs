84cef200ebd810e981419c525097204875f74293
Christhian Lopes <christhiancbl@gmail.com>
2026-04-19 00:51:27 -0300

    feat(product): add product entity

---
+++ src/OrderFlow.Domain/Entities/Entity.cs
@@ -0,0 +1,16 @@
+namespace OrderFlow.Domain.Entities;
+
+public abstract class Entity
+{
+    public Guid Id { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? UpdatedAt { get; private set; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void SetUpdated() => UpdatedAt = DateTime.UtcNow;
+}
